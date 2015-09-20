b8b07301b751ae5659882783509394f2a2b2f001
Efrain Ayllon <jesusayllonroa@gmail.com>
2015-09-20 12:42:46 -0500

    Working Copy

---
+++ ContentBlocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by Efrain Ayllon on 9/20/15.
+//  Copyright © 2015 Efrain Ayllon. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
