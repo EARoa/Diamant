45a79bbbff0451b9016361cb42397b8499352a65
Efrain Ayllon <jesusayllonroa@gmail.com>
2015-09-29 19:34:59 -0500

    White nav bar

--- Diamant/AppDelegate.swift
+++ Diamant/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        //White Navigation Bar
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
+        
         // Override point for customization after application launch.
         return true
     }
